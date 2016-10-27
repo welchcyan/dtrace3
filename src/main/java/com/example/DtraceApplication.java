ddbb4bae2ae183af26c200a6e3dc20786ba40966
chenwbj <chenwbj@gmail.com>
2016-10-27 17:13:10 +0800

    Initial commit

---
+++ src/main/java/com/example/DtraceApplication.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+public class DtraceApplication {
+
+	private static final Logger LOG = Logger.getLogger(DtraceApplication.class.getName());
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtraceApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		LOG.log(Level.INFO, "you called service4");
+		return "final call";
+	}
+
+
+}
